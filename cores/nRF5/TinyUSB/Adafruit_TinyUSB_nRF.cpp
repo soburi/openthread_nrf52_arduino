372c800dc642ec11e900ff37877abcf1d9e6a0df
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2020-07-18 11:49:46 +0900

    Use SoftDeviceHandler to process USB events

--- cores/nRF5/TinyUSB/Adafruit_TinyUSB_nRF.cpp
+++ cores/nRF5/TinyUSB/Adafruit_TinyUSB_nRF.cpp
@@ -30,6 +30,11 @@
 #include "Arduino.h"
 #include "Adafruit_TinyUSB_Core.h"
 
+#ifdef SOFTDEVICE_PRESENT
+#include "nrf_sdm.h"
+#include "nrf_sdh_soc.h"
+#endif
+
 //--------------------------------------------------------------------+
 // Forward USB interrupt events to TinyUSB IRQ Handler
 //--------------------------------------------------------------------+
@@ -54,7 +59,7 @@ extern "C" void USBD_IRQHandler(void)
 
 // tinyusb function that handles power event (detected, ready, removed)
 // We must call it within SD's SOC event handler, or set it as power event handler if SD is not enabled.
-extern "C" void tusb_hal_nrf_power_event(uint32_t event);
+extern "C" void tusb_hal_nrf_power_event(nrfx_power_usb_evt_t event);
 
 // USB Device Driver task
 // This top level thread process all usb events and invoke callbacks
@@ -70,6 +75,18 @@ static void usb_device_task(void* param)
   }
 }
 
+#ifdef SOFTDEVICE_PRESENT
+static void soc_evt_handler(uint32_t soc_evt, void * p_context)
+{
+    /*------------- usb power event handler -------------*/
+    int32_t usbevt = (soc_evt == NRF_EVT_POWER_USB_DETECTED   ) ? NRFX_POWER_USB_EVT_DETECTED:
+                     (soc_evt == NRF_EVT_POWER_USB_POWER_READY) ? NRFX_POWER_USB_EVT_READY   :
+                     (soc_evt == NRF_EVT_POWER_USB_REMOVED    ) ? NRFX_POWER_USB_EVT_REMOVED : -1;
+
+    if ( usbevt >= 0) tusb_hal_nrf_power_event((nrfx_power_usb_evt_t)usbevt);
+}
+#endif
+
 //--------------------------------------------------------------------+
 // Core Init & Touch1200
 //--------------------------------------------------------------------+
@@ -77,23 +94,43 @@ static void usb_device_task(void* param)
 // Init usb hardware when starting up. Softdevice is not enabled yet
 static void usb_hardware_init(void)
 {
-  // Power module init
-  const nrfx_power_config_t pwr_cfg = { 0 };
-  nrfx_power_init(&pwr_cfg);
-
-  // Register tusb function as USB power handler
-  const nrfx_power_usbevt_config_t config = { .handler = (nrfx_power_usb_event_handler_t) tusb_hal_nrf_power_event };
-  nrfx_power_usbevt_init(&config);
-
-  nrfx_power_usbevt_enable();
-
   // Priorities 0, 1, 4 (nRF52) are reserved for SoftDevice
   // 2 is highest for application
   NVIC_SetPriority(USBD_IRQn, 2);
 
   // USB power may already be ready at this time -> no event generated
   // We need to invoke the handler based on the status initially
-  uint32_t usb_reg = NRF_POWER->USBREGSTATUS;
+  uint32_t usb_reg;
+
+#ifdef SOFTDEVICE_PRESENT
+  NRF_SDH_SOC_OBSERVER(m_soc_observer_usb, NRF_SDH_SOC_STACK_OBSERVER_PRIO, soc_evt_handler, NULL);
+
+  uint8_t sd_en = false;
+  sd_softdevice_is_enabled(&sd_en);
+
+  if ( sd_en ) {
+    sd_power_usbdetected_enable(true);
+    sd_power_usbpwrrdy_enable(true);
+    sd_power_usbremoved_enable(true);
+
+    sd_power_usbregstatus_get(&usb_reg);
+  }else
+#endif
+  {
+    // Power module init
+    const nrfx_power_config_t pwr_cfg = { 0 };
+    nrfx_power_init(&pwr_cfg);
+
+    // Register tusb function as USB power handler
+    const nrfx_power_usbevt_config_t config = { .handler = (nrfx_power_usb_event_handler_t) tusb_hal_nrf_power_event };
+    nrfx_power_usbevt_init(&config);
+
+    nrfx_power_usbevt_enable();
+
+    // USB power may already be ready at this time -> no event generated
+    // We need to invoke the handler based on the status initially
+    usb_reg = NRF_POWER->USBREGSTATUS;
+  }
 
   if ( usb_reg & POWER_USBREGSTATUS_VBUSDETECT_Msk ) tusb_hal_nrf_power_event(NRFX_POWER_USB_EVT_DETECTED);
   if ( usb_reg & POWER_USBREGSTATUS_OUTPUTRDY_Msk  ) tusb_hal_nrf_power_event(NRFX_POWER_USB_EVT_READY);
