c13ea7b99c34a7d57fd0e6516eb1b009a631a8da
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2021-09-11 21:18:56 +0900

    tmp_remove

--- cores/nRF5/nrf_log.c
+++ cores/nRF5/nrf_log.c
@@ -1,5 +1,23 @@
 #include <stdbool.h>
+#include <stdint.h>
 
-void nrf_log_frontend_std_0() { }
-void nrf_log_frontend_std_1() { }
+void nrf_log_frontend_std_0(uint32_t severity_mid, char const * const p_str)
+{
+}
+
+
+void nrf_log_frontend_std_1(uint32_t            severity_mid,
+                            char const * const p_str,
+                            uint32_t           val0)
+{
+}
+
 bool nrf_log_frontend_dequeue() { return false; }
+void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
+{
+}
+
+bool ada_callback_invoke(const void* malloc_data, uint32_t malloc_len, const void* func, uint32_t arguments[], uint8_t argcount)
+{
+	return true;
+}
